4a26f2aef0cf4f499ebc5c683f50cb875fa10919
Garcia <magarc63@student.uiwtx.edu>
2026-04-23 16:36:06 -0500

    updated quickSelectionSet

--- src/tools/quickSelectionSet.py
+++ src/tools/quickSelectionSet.py
@@ -1,3 +1,25 @@
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout
 from core.MayaWidget import MayaWidget
 import maya.cmds as mc
+
+class QuickSelectSet():
+    def __init__(self):
+        pass
+
+class QuickSelectSetWidget(MayaWidget):
+    def __init__(self):
+        super().__init__()
+        self.quickSelectSet = QuickSelectSet()
+        self.setWindowTitle("QuickSelectSet")
+
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+
+        selectSetLayout = QHBoxLayout()
+        self.masterLayout.addLayout(selectSetLayout)
+
+def Run():
+    quickSelectSetWidget = QuickSelectSetWidget()
+    quickSelectSetWidget.show()
+
+Run()
